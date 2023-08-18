7026cacb2a4efdd6ce9251b7c074800b888e3135
Peter Ott at miami.zelmacorp.io <ottpeter@tuta.io>
2023-08-18 17:40:08 -0400

    mv files server

--- nextjs/app/api/action-plan-data-upload/route.ts
+++ nextjs/app/api/action-plan-data-upload/route.ts
@@ -8,18 +8,39 @@ export async function POST(request: NextRequest) {
     const cacheFolder = "./cache/";
     console.log(formData);
   
-    const title = formData.get('title');
-    const description = formData.get('description');
-    const documents = formData.get('documents');
-    const images = formData.get('images');
+    const title = formData.get('title');                    // Mandatory
+    const description = formData.get('description');        // Mandatory
+    const documents = formData.get('documents');            // Might or might not exist
+    const images = formData.get('images');                  // Might or might not exist
   
     if (!documents) {
-      fs.writeFileSync(cacheFolder + formData.get('documentName') + ".done", Date.now().toString());
+      fs.writeFileSync(cacheFolder + formData.get('documentName') + ".done", Date.now().toString());    // We signal to create-action-plan that we are done
       console.log("There are no documents attached for this ActionPlan.");
+    } else {
+      const documentFile: File = documents as File;
+      const filePath = `${cacheFolder}${documentFile.name}`;
+      const buffer = await documentFile.arrayBuffer();
+      
+      fs.writeFile(filePath, new DataView(buffer), () => {
+        fs.writeFileSync(cacheFolder + documentFile.name + ".done", Date.now().toString());             // We signal to create-action-plan that we are done
+        console.log('Document upload done.');
+      })
     }
+
     if (!images) {
-      fs.writeFileSync(cacheFolder + formData.get('imageName') + ".done", Date.now().toString());
+      fs.writeFileSync(cacheFolder + formData.get('imageName') + ".done", Date.now().toString());       // We signal to create-action-plan that we are done
       console.log("There are no images attached for this ActionPlan");
+    } else {
+      const imageFile: File = images as File;
+      const filePath = `${cacheFolder}${imageFile.name}`;
+      const buffer = await imageFile.arrayBuffer();
+      
+      const writeStream = fs.createWriteStream(filePath);
+
+      fs.writeFile(filePath, new DataView(buffer), () => {
+        fs.writeFileSync(cacheFolder + imageFile.name + ".done", Date.now().toString());                // We signal to create-action-plan that we are done
+        console.log("Image upload done.");
+      });
     }
     
     return NextResponse.json({
